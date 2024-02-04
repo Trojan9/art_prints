634120434b674b57474e56ec451a30a491c76dd5
trojan9 <timmybanjo@gmail.com>
2024-02-04 23:01:35 +0000

    Display menu and perform task

--- src/main/java/oop/ica/part1/ArtsPrintsSystem.java
+++ src/main/java/oop/ica/part1/ArtsPrintsSystem.java
@@ -14,17 +14,22 @@ public class ArtsPrintsSystem {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
+      
+        //display name and advertising phrase
         System.out.println("");
         System.out.println("Welcome to ART PRINTS,Where Walls Become Canvases: Art Prints for Every Space!");
         System.out.println("");
+        
+        //display the menu
         System.out.println("MENU: ");
-        int option=-1;
         System.out.println("");
         System.out.println("1. View");
         System.out.println("2. Buy");
         System.out.println("3. Add");
         System.out.println("0. Quit");
         
+        //listen to keyboard and perform task
+        int option=-1;
         while(option !=0){
             option= keyboard.nextInt();
             switch (option) {
@@ -43,8 +48,10 @@ public class ArtsPrintsSystem {
                 default -> {
                 }
             }
-            
+            //prints menu out if option is not to quit at the end of the task   
             if(option!=0){
+                System.out.println("");
+                System.out.println("MENU: ");
                 System.out.println("");
                 System.out.println("1. View");
                 System.out.println("2. Buy");
