226a60b8b97a68110822496844c290d357981fca
trojan9 <timmybanjo@gmail.com>
2024-02-04 22:14:49 +0000

    rename project

---
+++ src/main/java/oop/ica/part1/ArtsPrintsSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package oop.ica.part1;
+
+/**
+ *
+ * @author oluwatimilehinbanjo
+ */
+public class ArtsPrintsSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
