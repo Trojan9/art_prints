a6d0eb1a071281e92d2321d65ceb794043ccde97
trojan9 <timmybanjo@gmail.com>
2024-02-04 22:52:37 +0000

    new push man

--- src/main/java/oop/ica/part1/ArtsPrintsSystem.java
+++ src/main/java/oop/ica/part1/ArtsPrintsSystem.java
@@ -4,6 +4,8 @@
 
 package oop.ica.part1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author oluwatimilehinbanjo
@@ -11,6 +13,45 @@ package oop.ica.part1;
 public class ArtsPrintsSystem {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("");
+        System.out.println("Welcome to ART PRINTS,Where Walls Become Canvases: Art Prints for Every Space!");
+        System.out.println("");
+        System.out.println("MENU: ");
+        int option=-1;
+        System.out.println("");
+        System.out.println("1. View");
+        System.out.println("2. Buy");
+        System.out.println("3. Add");
+        System.out.println("0. Quit");
+        
+        while(option !=0){
+            option= keyboard.nextInt();
+            switch (option) {
+                case 1 -> {
+                    System.out.println("");
+                    System.out.println("View");
+                }
+                case 2 -> {
+                    System.out.println("");
+                     System.out.println("Buy");
+                }
+                case 3 -> {
+                    System.out.println("");
+                     System.out.println("Add");
+                }
+                default -> {
+                }
+            }
+            
+            if(option!=0){
+                System.out.println("");
+                System.out.println("1. View");
+                System.out.println("2. Buy");
+                System.out.println("3. Add");
+                System.out.println("0. Quit");
+            }
+            
+        }
     }
 }
