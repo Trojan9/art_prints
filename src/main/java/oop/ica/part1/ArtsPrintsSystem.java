c3d19a6c95b312a5a92ca2531ffeb13b0c701135
trojan9 <timmybanjo@gmail.com>
2024-02-12 20:35:08 +0000

    optimized code

--- src/main/java/oop/ica/part1/ArtsPrintsSystem.java
+++ src/main/java/oop/ica/part1/ArtsPrintsSystem.java
@@ -17,60 +17,74 @@ import java.util.Scanner;
 public class ArtsPrintsSystem {
 
     public static void main(String[] args) {
+        
         // Create instance of Scanner class for taking user input
         Scanner keyboard = new Scanner(System.in);
 
         // Display name and advertising phrase
-        System.out.println("ART PRINTS");
-        System.out.println("Welcome to ART PRINTS, Where Walls Become Canvases: Art Prints for Every Space!");
-        System.out.println("");
+        welcome();
 
-        // Display the menu
-        System.out.println("MENU: ");
-        System.out.println("\n 1. View \n 2. Buy \n 3. Add \n 0. Quit ");
-         
+        
+        //declare local variables
         int option = -1;
+        
+        //start while loop
         while (option != 0) {
-
+            
+            // Display the menu
+            if (option != 0) {
+                menu();
+            }
+            
             // Using a try catch block to handle any exceptions that might occur from non-integer inputs
             try {
                 
-                // listen to keyboard and perform task
+                // listen to keyboard input
                 option = keyboard.nextInt();
                 
+                //process choice
                 switch (option) {
-                    case 0 -> {
-                        System.out.print("Thanks for using our services, see you next time!");
-                    }
-                    case 1 -> {
-                        System.out.println("Your selected option is: ");
-                        System.out.println("View");
-                    }
-                    case 2 -> {
-                        System.out.println("Your selected option is: ");
-                        System.out.println("Buy");
-                    }
-                    case 3 -> {
-                        System.out.println("Your selected option is: ");
-                        System.out.println("Add");
-                    }
-
-                    default -> {
-                        System.out.println("Incorrect option, please try again!");
-                    }
-                }
+                    case 0 -> System.out.print("Thanks for using our services, see you next time!");
+                    
+                    case 1 ->display("View");
+                    
+                    case 2 ->display("Buy");
+                    
+                    case 3 ->display("Add");
+                    
+                    default -> System.out.println("Incorrect option, please try again!");
+                    
+                }//end of switch
+                
             } 
             catch (InputMismatchException e) {
-                System.out.println("Incorrect input, please enter a number from the menu options");
+                //validation method
+                //if value is not an integer, error is thrown
+                System.err.println("Incorrect input, please enter a number from the menu options");
                 keyboard.next();
             }
-
-            // prints menu out if option is not to quit at the end of the task
-            if (option != 0) {
-                System.out.println("");
-                System.out.println("MENU: ");
-                System.out.println("\n 1. View \n 2. Buy \n 3. Add \n 0. Quit ");
-            }   
-        }
-    }
+            
+            
+        }//end of while loop
+        
+    }//end of main
+    
+    
+    static void menu(){
+        // Display the menu
+        System.out.println("\nMENU: ");
+        System.out.println("\n 1. View \n 2. Buy \n 3. Add \n 0. Quit ");
+    }//end of menu function
+    
+    static void welcome(){
+        // Display name and advertising phrase
+        System.out.println("ART PRINTS");
+        System.out.println("Welcome to ART PRINTS, Where Walls Become Canvases: Art Prints for Every Space!");
+    }//end of welcome function
+    
+    static void display(String choice){
+        // Display choice
+        System.out.println("Your selected option is: ");
+        System.out.println(choice);
+    }//end of display function
 }
